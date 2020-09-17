9fb6cc59f0b314b6065949af359851eb6d778e6f
andvetall13 <andvetall13@gmail.com>
2020-09-17 17:46:16 +0300

    few changes

--- src/app/pages/client/dashboard/components/3d-model/engine/engine.service.ts
+++ src/app/pages/client/dashboard/components/3d-model/engine/engine.service.ts
@@ -15,11 +15,12 @@ export class EngineService implements OnDestroy {
   private renderer: THREE.WebGLRenderer;
   private camera: THREE.PerspectiveCamera;
   private scene: THREE.Scene;
-  private light: THREE.AmbientLight;
-  private light1: THREE.AmbientLight;
-  private light2: THREE.AmbientLight;
-
-  private light3: THREE.Light
+  private hlight: THREE.AmbientLight;
+  private light: THREE.PointLight;
+  private light2: THREE.PointLight;
+  private light3: THREE.PointLight;
+  private light4: THREE.PointLight;
+  private directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
 
   private cube: THREE.Mesh;
 
@@ -41,12 +42,7 @@ export class EngineService implements OnDestroy {
       let arr = [...model];
       arr.forEach(item => {
         this.loader.load( item, gltf => {
-          console.log(gltf.scene);
-          
-          // gltf.scene.children[0].scale.set(2, 2, 2)
-          // gltf.scene.children[3].scale.set(2, 2, 2)
-          // gltf.scene.children[4].scale.set(2, 2, 2)
-    
+          gltf.scene.children[0].scale.set(0.5,0.5,0.5);
            this.scene.add(gltf.scene);
          }, err => err );
       })
@@ -61,22 +57,20 @@ export class EngineService implements OnDestroy {
       alpha: true,    // transparent background
       antialias: true 
     });
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setSize(857, 483);
 
     this.scene = new THREE.Scene();
 
     // пол сцены!
     let mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
     this.scene.add( mesh );
-    let grid = new THREE.GridHelper( 100, 40, 0x000000, 0x000000 );
+    let grid = new THREE.GridHelper( 10000, 4000, 0x999999, 0x999999 );
     this.scene.add( grid );
 
 
-    this.scene.fog = new THREE.Fog( 0x91a3b0 , 0, 120 );
-
-    this.camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 500)
-    this.camera.position.set( 0,4,8 );
-    this.camera.lookAt( new THREE.Vector3( ) );
+    this.camera = new THREE.PerspectiveCamera(25, 857 / 483, 10, 1500)
+    this.camera.position.set( 500,150,50 );
+    this.camera.lookAt( new THREE.Vector3() );
     this.scene.add(this.camera);
     
     this.controls = new OrbitControls( this.camera, this.renderer.domElement );
@@ -99,26 +93,32 @@ export class EngineService implements OnDestroy {
       }
       this.controls.screenSpacePanning = true
 
-    this.light = new THREE.AmbientLight( 0x3a5050 );
-    this.light.position.set(-500,100,500);
-    this.scene.add(this.light);
+      this.hlight = new THREE.AmbientLight (0x0f0f0f,400);
+      this.scene.add(this.hlight);
 
-    this.light1 = new THREE.AmbientLight( 0x576b6e );
-    this.light1.position.set(-200,100,400);
-    this.scene.add(this.light1);
+      this.directionalLight = new THREE.DirectionalLight(0xffffff,0.5);
+      this.directionalLight.position.set(0,1,0);
+      this.directionalLight.castShadow = true;
+      this.scene.add(this.directionalLight);
 
-    this.light2 = new THREE.AmbientLight( 0x404040 ); // soft white light
-    this.light2.position.set(-500,100,500);
-    this.scene.add( this.light2 );
+      this.light = new THREE.PointLight(0xc4c4c4,2);
+      this.light.position.set(0,300,500);
+      this.scene.add(this.light);
 
-    this.light3 = new THREE.Light('#f50a26') 
-    this.light3.position.set(-500,100,500);
-    this.scene.add( this.light3 );
-    
+      this.light2 = new THREE.PointLight(0xc4c4c4,2);
+      this.light2.position.set(500,100,0);
+      this.scene.add(this.light2);
+
+      this.light3 = new THREE.PointLight(0xc4c4c4,2);
+      this.light3.position.set(0,100,-500);
+      this.scene.add(this.light3);
+      
+      this.light4 = new THREE.PointLight(0xc4c4c4,2);
+      this.light4.position.set(-500,300,500);
+      this.scene.add(this.light4);
 
     this.multiLoad (
-      '../../../../../../../assets/3d/scene.gltf', 
-      '../../../../../../../assets/3d/uploads_files_2226358_STOOL.glb'
+      '../../../../../../../assets/3d/scene.gltf'
     )
     
   }
@@ -149,9 +149,8 @@ export class EngineService implements OnDestroy {
   }
 
   public resize(): void {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-
+    const width = 857;
+    const height = 483;
     this.camera.aspect = width / height;
     this.camera.updateProjectionMatrix();
 

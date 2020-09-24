516e06f58779c95f01b2d3ca609530f6211979f6
andvetall13 <andvetall13@gmail.com>
2020-09-24 15:14:40 +0300

    test

--- src/app/pages/client/dashboard/components/3d-model/engine/engine.component.ts
+++ src/app/pages/client/dashboard/components/3d-model/engine/engine.component.ts
@@ -5,34 +5,69 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
 @Component({
   selector: 'app-engine',
-  templateUrl: './engine.component.html'
+  templateUrl: './engine.component.html',
+  styleUrls: ['./engine.component.scss']
 })
-export class EngineComponent implements OnInit, AfterViewInit {
+export class EngineComponent implements OnInit {
 
   // @ViewChild('rendererCanvas', null) rendererCanvas: ElementRef;
   public scene: THREE.Scene;
   public camera: THREE.PerspectiveCamera;
   public renderer: THREE.WebGLRenderer;
   public controls : OrbitControls;
+  public on: boolean = false;
+  public animationPlay: boolean = false;
+  
+  public array: any = [
+    {
+      texture_ft: new THREE.TextureLoader().load( "../../../../../../../assets/3d/arid2_ft.jpg"),
+      texture_bk: new THREE.TextureLoader().load( '../../../../../../../assets/3d/arid2_bk.jpg'),
+      texture_up: new THREE.TextureLoader().load( '../../../../../../../assets/3d/arid2_up.jpg'),
+      texture_dn: new THREE.TextureLoader().load( '../../../../../../../assets/3d/arid2_dn.jpg'),
+      texture_rt: new THREE.TextureLoader().load( '../../../../../../../assets/3d/arid2_rt.jpg'),
+      texture_lf: new THREE.TextureLoader().load( '../../../../../../../assets/3d/arid2_lf.jpg')
+    },
+    {
+      texture_ft: new THREE.TextureLoader().load( "../../../../../../../assets/3d/arid_ft.jpg"),
+      texture_bk: new THREE.TextureLoader().load( '../../../../../../../assets/3d/arid_bk.jpg'),
+      texture_up: new THREE.TextureLoader().load( '../../../../../../../assets/3d/arid_up.jpg'),
+      texture_dn: new THREE.TextureLoader().load( '../../../../../../../assets/3d/arid_dn.jpg'),
+      texture_rt: new THREE.TextureLoader().load( '../../../../../../../assets/3d/arid_rt.jpg'),
+      texture_lf: new THREE.TextureLoader().load( '../../../../../../../assets/3d/arid_lf.jpg')
+    },
+    {
+      texture_ft: new THREE.TextureLoader().load( "../../../../../../../assets/3d/overcast_ft.jpg"),
+      texture_bk: new THREE.TextureLoader().load( '../../../../../../../assets/3d/overcast_bk.jpg'),
+      texture_up: new THREE.TextureLoader().load( '../../../../../../../assets/3d/overcast_up.jpg'),
+      texture_dn: new THREE.TextureLoader().load( '../../../../../../../assets/3d/overcast_dn.jpg'),
+      texture_rt: new THREE.TextureLoader().load( '../../../../../../../assets/3d/overcast_rt.jpg'),
+      texture_lf: new THREE.TextureLoader().load( '../../../../../../../assets/3d/overcast_lf.jpg')
+    },
+    {
+      texture_ft: new THREE.TextureLoader().load( "../../../../../../../assets/3d/quirk_ft.jpg"),
+      texture_bk: new THREE.TextureLoader().load( '../../../../../../../assets/3d/quirk_bk.jpg'),
+      texture_up: new THREE.TextureLoader().load( '../../../../../../../assets/3d/quirk_up.jpg'),
+      texture_dn: new THREE.TextureLoader().load( '../../../../../../../assets/3d/quirk_dn.jpg'),
+      texture_rt: new THREE.TextureLoader().load( '../../../../../../../assets/3d/quirk_rt.jpg'),
+      texture_lf: new THREE.TextureLoader().load( '../../../../../../../assets/3d/quirk_lf.jpg')
+    },
+  ]
+  public index: number = 0;
 
   public constructor(
-    // private engServ: EngineService
     ) {}
 
   public ngOnInit(): void {
-    // this.engServ.createScene(this.rendererCanvas);
-    // this.engServ.animate();
-    this.createScene();
+    // this.createScene();
   }
   public createScene(){
     this.scene = new THREE.Scene()
-    this.camera = new THREE.PerspectiveCamera(50, 857 / 483, 10, 30000);
+    this.camera = new THREE.PerspectiveCamera(50, 1138 / 641, 10, 30000);
       this.camera.position.set(0,0,0);
       this.renderer = new THREE.WebGLRenderer({
         antialias:true
       });
-      this.renderer.setSize(857, 483);
-      // this.rendererCanvas.nativeElement.append(this.renderer.domElement);
+      this.renderer.setSize(1138, 641);
       document.querySelector('.engine-wrapper').appendChild(this.renderer.domElement);
       
       this.controls = new OrbitControls(this.camera, this.renderer.domElement);
@@ -40,33 +75,15 @@ export class EngineComponent implements OnInit, AfterViewInit {
       this.controls.maxDistance = 2000;
 
       let materialArray = [];
-      let texture_ft = new THREE.TextureLoader().load( "../../../../../../../assets/3d/arid2_ft.jpg");
-      let texture_bk = new THREE.TextureLoader().load( '../../../../../../../assets/3d/arid2_bk.jpg');
-      let texture_up = new THREE.TextureLoader().load( '../../../../../../../assets/3d/arid2_up.jpg');
-      let texture_dn = new THREE.TextureLoader().load( '../../../../../../../assets/3d/arid2_dn.jpg');
-      let texture_rt = new THREE.TextureLoader().load( '../../../../../../../assets/3d/arid2_rt.jpg');
-      let texture_lf = new THREE.TextureLoader().load( '../../../../../../../assets/3d/arid2_lf.jpg');
-      
-      materialArray.push(new THREE.MeshBasicMaterial( { map: texture_ft }));
-      materialArray.push(new THREE.MeshBasicMaterial( { map: texture_bk }));
-      materialArray.push(new THREE.MeshBasicMaterial( { map: texture_up }));
-      materialArray.push(new THREE.MeshBasicMaterial( { map: texture_dn }));
-      materialArray.push(new THREE.MeshBasicMaterial( { map: texture_rt }));
-      materialArray.push(new THREE.MeshBasicMaterial( { map: texture_lf }));
-        
+      for( let item in this.array[this.index]){
+        materialArray.push(new THREE.MeshBasicMaterial( { map: this.array[this.index][item] }))
+      }
         
       for (let i = 0; i < 6; i++){
         materialArray[i].side = THREE.BackSide;
       }
-        
-      // let skyboxGeo = new THREE.BoxGeometry( 10000, 10000, 10000);
-      // let skybox = new THREE.Mesh( skyboxGeo, materialArray );
-      
-      // this.scene.add( skybox );
       const geometry = new THREE.BoxGeometry( 5000, 5000 , 5000, 6, 6, 6 );
-      // const material = new THREE.MeshBasicMaterial( {color: 0xffffff, wireframe: true} );
       const sphere = new THREE.Mesh( geometry, materialArray );
-      // sphere.material = new THREE.MeshBasicMaterial( {color: 0xaaaabb, wireframe: true})
       this.scene.add( sphere );
       this.animate();
     }
@@ -74,8 +91,57 @@ export class EngineComponent implements OnInit, AfterViewInit {
       this.renderer.render(this.scene,this.camera);
       requestAnimationFrame(this.animate.bind(this));
     }
-    ngAfterViewInit(): void {
-      // this.createScene();
+
+    public onOff(){
+      if(!this.on){
+        this.on = !this.on;
+        let selection = document.querySelector('.engine-wrapper')
+        selection.childNodes.forEach((item) => {
+          if(item.nodeName === "CANVAS"){
+            selection.removeChild(item)
+          }
+        })
+        this.createScene();
+        return
+      }
+      this.animationPlay = true;
+      setTimeout(() => {
+        let selection = document.querySelector('.engine-wrapper')
+        selection.childNodes.forEach((item) => {
+          if(item.nodeName === "CANVAS"){
+            selection.removeChild(item)
+          }
+        })
+        this.animationPlay = false;
+        this.on = !this.on;
+        this.index = 0;
+      }, 3500)
+    }
+    public plus(){
+      if(this.index < this.array.length - 1 && this.on){
+        let selection = document.querySelector('.engine-wrapper')
+        selection.childNodes.forEach((item) => {
+          if(item.nodeName === "CANVAS"){
+            selection.removeChild(item)
+          }
+        })
+        this.index++;
+        this.createScene();
+        return;
+      }
+    }
+    public minus(){
+      if(this.index > 0 && this.on){
+        let selection = document.querySelector('.engine-wrapper')
+        selection.childNodes.forEach((item) => {
+          if(item.nodeName === "CANVAS"){
+            selection.removeChild(item)
+          }
+        })
+        this.index--;
+        this.createScene();
+        return;
+      }
     }
 
 }
